dc9708d48b2fbe35f4639690456491f4094d8c4c
Mesalina23 <mebrapu23@gmail.com>
2021-10-04 09:54:28 +0200

    readme

--- src/components/App.js
+++ src/components/App.js
@@ -46,6 +46,15 @@ function App() {
     (character) => character.id === parseInt(characterId)
   );
   const filteredData = data
+    .sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+      return 0;
+    })
     .filter((character) =>
       character.name
         .toLocaleLowerCase()
