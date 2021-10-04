c62250f7525aeafc0f1925d029c5b8c79867478d
Mesalina23 <mebrapu23@gmail.com>
2021-10-04 15:59:04 +0200

    create icons

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -1,5 +1,26 @@
 import { Link } from 'react-router-dom';
+import alien from '../images/alien.png';
+import brain from '../images/brain.png';
+import heart from '../images/heartbeat.png';
+import tomb from '../images/tombstone.png';
+import unknown from '../images/question-mark.png';
 function CharacterItem(props) {
+  const specie = () => {
+    if (props.characterData.species === 'Human') {
+      return brain;
+    } else if (props.characterData.species === 'Alien') {
+      return alien;
+    }
+  };
+  const status = () => {
+    if (props.characterData.status === 'Alive') {
+      return heart;
+    } else if (props.characterData.status === 'Dead') {
+      return tomb;
+    } else {
+      return unknown;
+    }
+  };
   return (
     <Link
       to={`./character/${props.characterData.id}`}
@@ -13,8 +34,17 @@ function CharacterItem(props) {
       />
       <article className='card__article'>
         <h4 className='card__article--title'>{props.characterData.name}</h4>
-        <p className='card__article--specie'>{props.characterData.species}</p>
-        <p>{props.characterData.status}</p>
+        <img
+          src={specie()}
+          alt={props.characterData.specie}
+          className='card__article--img'
+        ></img>
+        <img
+          src={status()}
+          alt={props.characterData.status}
+          className='card__article--img'
+        ></img>
+        <p></p>
       </article>
     </Link>
   );
